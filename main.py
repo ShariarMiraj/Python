4a1a983afc99e28f91ad8a239072920da973a0eb
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-10-27 15:07:18 +0000

    add3

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+# This miraj code ..plz dont delete
 print("Helo ... I am miraj")
 print(5,77)
 print(7*2)
+
+#Author: Miraj Shahriar 
+#Course: Python 
+
+print("hey i am Shahriar \nThis is my first python code")
+#This is a single line comment 
+print("This is a multiline comment")
+print("\"Welcomle\" This pyhton Course ")
+
+
+# This is Separated  character/ paramter 
+print("Hey " , 6 , 7 , sep="~" , end="009\n")
+print("Find  " , 9 , 7 , sep="~" , end="009")
+
+
+
+
+
+
+
