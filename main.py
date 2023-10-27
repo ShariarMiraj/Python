d1bfef343bdc7a369541641bf240b3fe02c3daab
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-10-27 15:37:10 +0000

    addd4

--- main.py
+++ main.py
@@ -1,24 +1,61 @@
 # This miraj code ..plz dont delete
 print("Helo ... I am miraj")
-print(5,77)
-print(7*2)
+print(5, 77)
+print(7 * 2)
 
-#Author: Miraj Shahriar 
-#Course: Python 
+#Author: Miraj Shahriar
+#Course: Python
 
 print("hey i am Shahriar \nThis is my first python code")
-#This is a single line comment 
+#This is a single line comment
 print("This is a multiline comment")
 print("\"Welcomle\" This pyhton Course ")
 
+# This is Separated  character/ paramter
+print("Hey ", 6, 7, sep="~", end="009\n")
+print("Find  ", 9, 7, sep="~", end="009")
 
-# This is Separated  character/ paramter 
-print("Hey " , 6 , 7 , sep="~" , end="009\n")
-print("Find  " , 9 , 7 , sep="~" , end="009")
+print("\n\n\n")
 
+# Variables And Data Types
+a = 1
+b = "Python"
+c = None
+d = 3.14
+e = 3 + 4j
+f = True
+g = b + " is awesome"
+h = complex(8, 2)
+print(a, b, c, d, e, f, g, h)
 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
 
+print("\n\n\n")
 
+# List
+list = ["python", "java", "c++", "c#", "c"]
+print(list)
+print(type(list))
 
+list1 = [8, 3.2], [-4, 5], ["APPLE"]
+print(list1)
+print(type(list1))
 
+print("\n\n\n")
+# dictioanry
+dict = {"name": "Miraj", "age": 23, "city": "Pune"}
+print(dict)
+print(type(dict))
 
+print("\n\n\n")
+# tuple
+tup = ("python", "java", "c++", "c#", "c")
+print(tup)
+print(type(tup))
