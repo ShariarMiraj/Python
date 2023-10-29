14331aeebe850e9be1612b0bd1fef871ecb2bf0d
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-10-29 15:35:24 +0000

    adddd9

---
+++ string.py
@@ -0,0 +1,24 @@
+name = "Python"
+print("helo" , name)
+# Output: helo Python
+print("\n")
+
+# 3 cot and single cot
+anotherfriend= '''Hi I am .Net
+hi c++
+hi java
+"hi sql"
+hi python'''
+print(anotherfriend)
+# Output: Hi I am .Net
+# hi c++
+# hi java
+# hi python  
+
+
+# Indexing ( python is like a arry of characater)
+
+naam = "Python"
+print(naam[0])
+print(naam[1])
+# Output: P
