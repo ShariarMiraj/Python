d47ffd0204fb93878800a74a5b2fae1749497b13
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-10-29 15:15:00 +0000

    addd8

--- Typecasting.py
+++ Typecasting.py
@@ -1,22 +1,20 @@
-#..........Explicit................. 
+#..........Explicit.................
 
 # string convert in integer #Exmaple 1
-a="1"
-b="2"
+a = "1"
+b = "2"
 # print(a+b)
-print(int(a)+int(b))
+print(int(a) + int(b))
 
 #Example 2 of typecasting
-string ="15"
-num=7
+string = "15"
+num = 7
 string_num = int(string)
-sum= num + string_num
-print("sum the both number" , sum)
-
+sum = num + string_num
+print("sum the both number", sum)
 
 #..........Implicit.................
 
-A=1.5
-B=7
-print(A+B)
-
+A = 1.5
+B = 7
+print(A + B)
---
+++ userinp.py
@@ -0,0 +1,14 @@
+a=input("Enter Your Name :")
+b=input("Enter Your Age :")
+c=input("Enter Your Address :")
+
+print("Name is :",a)
+print("Age is :",b)
+print("Address is :",c)
+
+
+x= input("Enter your first number : ")
+y= input("Enter your second number : ")
+print(x+y)
+
+print(int(x)+int(y))
