2f89ef157ada55c6888ee8d4d67b86a2226317c6
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-10-29 16:02:22 +0000

    adddd10

---
+++ forloop.py
@@ -0,0 +1,11 @@
+anotherfriend= '''Hi I am .Net
+hi c++
+hi java
+"hi sql"
+hi python'''
+print(anotherfriend)
+
+# Letes print for loop 
+for i in anotherfriend:
+  print(i)
+
--- string.py
+++ string.py
@@ -15,10 +15,48 @@ print(anotherfriend)
 # hi java
 # hi python  
 
+#////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\//////
 
 # Indexing ( python is like a arry of characater)
 
 naam = "Python"
 print(naam[0])
-print(naam[1])
-# Output: P
+print(naam[1])# Output: P y
+
+
+# slicing ( slice the string)
+print(naam[0:2])
+# Output: Py
+name="miraj , singh"
+print(name[0:3])
+
+
+#////////////////////////////////////////////////
+# Lenght of string
+print(len(name))
+# Output: 13
+
+fruit = "Python"
+len1 = len(fruit)
+print("python is ", len1, "word")
+# Output: 6
+
+#////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+#String arry and Slicing 
+
+
+fruit = "Mango"
+len1 = len(fruit)
+print(len1)
+print(fruit[0:3])
+
+nm="MIRAJ"
+print(nm[-4:-2])
+
+
+
+
+
