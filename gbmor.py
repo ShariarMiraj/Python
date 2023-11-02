a505ba0a3c61b83cbab1fe22ad003fe2ea0b1827
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-02 13:48:37 +0000

    adddd18

---
+++ gbmor.py
@@ -0,0 +1,29 @@
+time= int(input("Enter the time: "))
+if (time<12):
+  print("GODD MOring every one")
+
+elif(time>12 and time<17):
+  print("GOOD Afternoon every one ")
+elif(time>17 and time<24):
+  print("GOOD Night every one ")
+elif(time>24):
+  print("This worng input my dear. plz enter the right timee ........ ")
+  
+else:
+  print("Have a nice a day guys ")
+
+# # this is import timme 
+# # import time
+# import time 
+# timestamp = time.strftime("%H:%M:%S")
+# print(timestamp)
+# timestamp = int(time.strftime("%H"))
+# print(timestamp)
+# timestamp = time.strftime("%M")
+# print(timestamp)
+# timestamp = time.strftime("%S")
+# print(timestamp)
+# timestamp = time.strftime("%d")
+# print(timestamp)
+# timestamp = time.strftime("%m")
+# print(timestamp)
