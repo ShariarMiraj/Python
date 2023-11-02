63071831e71dafa35e24be62f7169d8d90daa0b4
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-02 13:29:07 +0000

    adddd17

---
+++ 3ifels.py
@@ -0,0 +1,16 @@
+#Nasted if else
+
+num = int(input("Enter a number: "))
+if (num <0):
+  print("Number is negative")
+
+elif (num > 0):
+  if(num <=10):
+    print("Number is between 0 and 10")
+  elif(num > 10 and num <=20):
+    print("Number is between 10 and 20")
+  else:
+    print("Number is greater than 20")
+    
+else:
+  print("Number is zero")
