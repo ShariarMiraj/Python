d2a9904f5ddb9c705180616c26fab3a5805e140a
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-02 12:43:43 +0000

    adddd14

--- Todolist.py
+++ Todolist.py
@@ -1,4 +1,4 @@
-tasks = []
+tasks = [] 
 
 
 def add_task():
---
+++ ifels.py
@@ -0,0 +1,7 @@
+a =int(input("Enter a AGe:"))
+print("your age is : ",a )
+
+if a>=18:
+    print("you are eligible for voting")
+else:
+    print("you are not eligible for voting")
