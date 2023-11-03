ddc70765a6d4cfe9aa074dc703ed25799557be7a
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-03 12:19:34 +0000

    adddd21
    For Loop

---
+++ 1floop.py
@@ -0,0 +1,17 @@
+# name = "Miraj"
+# for i in name:
+#   print(i)
+#   if (i == "a"):
+#     print("This is somthing")
+
+
+colors = ["red", "blue", "green", "yellow", "orange", "purple"]
+for color in colors:
+  print(color)
+  # for i in color:
+  #   print(i)
+  if (color == "red"):
+    print("This is somthing")
+    
+    
+  
