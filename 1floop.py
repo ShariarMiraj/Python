491e8e7f3837ab8824c4c36aaa88b5805b19b00b
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-03 12:30:15 +0000

    addd23

--- 1floop.py
+++ 1floop.py
@@ -1,8 +1,8 @@
-# name = "Miraj"
-# for i in name:
-#   print(i)
-#   if (i == "a"):
-#     print("This is somthing")
+name = "Miraj"
+for i in name:
+  print(i)
+  if (i == "a"):
+    print("This is somthing")
 
 
 colors = ["red", "blue", "green", "yellow", "orange", "purple"]
@@ -13,13 +13,13 @@ for color in colors:
   if (color == "red"):
     print("This is somthing")
 
-# k = int(input("Enter a number: "))
-# for k in range(10):
-#   print(k +1)
+k = int(input("Enter  number 10: "))
+for k in range(10):
+  print(k +1)
   
-# k = int(input("Enter a number: "))
-# for k in range(1, 12, 3):
-#   print(k)
+k = int(input("Enter a number: "))
+for k in range(1, 12, 3):
+  print(k)
     
     
   
