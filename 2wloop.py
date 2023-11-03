8fbdcc123c134746610d068f139fbce8b6be0e8a
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-03 12:57:53 +0000

    addddd26

--- 2wloop.py
+++ 2wloop.py
@@ -3,12 +3,18 @@
 #   print(i)
 #   i += 1
 
-i = int(input("Enter a number: "))
-while(i<25):
-  i = int(input("Enter a number: "))
-  print(i)
+# i = int(input("Enter a number: "))
+# while(i<25):
+#   i = int(input("Enter a number: "))
+#   print(i)
 
+# print("Done with the loop :")
+
+
+count =5
+while (count >0):
+  print(count)
+  count = count - 1
 print("Done with the loop :")
 
 
-  
