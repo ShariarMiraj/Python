a0194e6ab6eae2fed2a0c6682fe0b023b5850cf4
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-04 13:55:59 +0000

    adddd31

--- fun.py
+++ fun.py
@@ -1,14 +1,27 @@
+# Functions in Python
+
 def calculationGmean(a,b):
   mean = (a*b)/(a+b)
   print(mean)
   
 a = 6
 b = 2
-gmean1 = (a*b)/(a+b)
-print(gmean1)
-c=8
-d=7
+if(a>b):
+  print("First Number is Greater number ")
+else:
+  print("Second Number is Greater number ")
+calculationGmean(a,b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
 
+print("\n")
 
-gmean2 = (a+b)/(a-b)
-print(gmean2)
+c=12
+d=2
+if(c>d):
+  print("First Number is Greater number ")
+else:
+  print("Second Number is Greater number ")
+calculationGmean(c,d)
+# gmean2 = (a+b)/(a-b)
+# print(gmean2)
