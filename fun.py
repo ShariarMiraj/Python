dd99bfdf8a8cc654d5a84b4e4a5d435e2f0064a6
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-04 14:29:52 +0000

    adddd

--- fun.py
+++ fun.py
@@ -1,54 +1,60 @@
 # Functions in Python
 
-def calculationGmean(a,b):
-  mean = (a*b)/(a+b)
+
+def calculationGmean(a, b):
+  mean = (a * b) / (a + b)
   print(mean)
-  
+
+
 a = 6
 b = 2
-if(a>b):
+if (a > b):
   print("First Number is Greater number ")
 else:
   print("Second Number is Greater number ")
-calculationGmean(a,b)
+calculationGmean(a, b)
 # gmean1 = (a*b)/(a+b)
 # print(gmean1)
 
 print("\n")
 
-c=12
-d=2
-if(c>d):
+c = 12
+d = 2
+if (c > d):
   print("First Number is Greater number ")
 else:
   print("Second Number is Greater number ")
-calculationGmean(c,d)
+calculationGmean(c, d)
 # gmean2 = (a+b)/(a-b)
 # print(gmean2)
 
-
 print("\n")
-# Other way less And Greater
-def calculationGmean1(a,b):
-    mean = (a*b)/(a+b)
-    print(mean)
 
-def isGreater(a,b):
-  if(a>b):
+
+# Other way less And Greater
+def calculationGmean1(a, b):
+  mean = (a * b) / (a + b)
+  print(mean)
+
+
+def isGreater(a, b):
+  if (a > b):
     print("First Number is Greater number ")
   else:
     print("Second Number is Greater number ")
+
 
 def isLesser(a, b):
   pass
   #        (pass means -> I work lttr in this functions)
 
-a = 8 
-b= 6
-isGreater(a,b)
-calculationGmean1(a,b)
 
-c=12
-d=14
-isGreater(c,d)
-calculationGmean1(c,d)
+a = 8
+b = 6
+isGreater(a, b)
+calculationGmean1(a, b)
+
+c = 12
+d = 14
+isGreater(c, d)
+calculationGmean1(c, d)
---
+++ fun2.py
@@ -0,0 +1,12 @@
+# Defult fanction
+
+def avarege(a =5,b=2):
+  print("The avarege of ", a, " and ", b, " is : ", (a+b)/2)
+
+avarege(5,2)
+avarege(b=5,a=2)
+avarege(a=5,b=2)
+avarege(a=5,b=5)
+avarege()
+
+  
