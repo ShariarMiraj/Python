f11011f991787fa8715b1860c16425a5d898dead
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-04 14:07:05 +0000

    adddd32

--- fun.py
+++ fun.py
@@ -25,3 +25,30 @@ else:
 calculationGmean(c,d)
 # gmean2 = (a+b)/(a-b)
 # print(gmean2)
+
+
+print("\n")
+# Other way less And Greater
+def calculationGmean1(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+  if(a>b):
+    print("First Number is Greater number ")
+  else:
+    print("Second Number is Greater number ")
+
+def isLesser(a, b):
+  pass
+  #        (pass means -> I work lttr in this functions)
+
+a = 8 
+b= 6
+isGreater(a,b)
+calculationGmean1(a,b)
+
+c=12
+d=14
+isGreater(c,d)
+calculationGmean1(c,d)
