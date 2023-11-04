415987886f8bc88e7b3976330e8d9cec8e4816c9
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-04 13:30:51 +0000

    adddd29

--- bc.py
+++ bc.py
@@ -7,7 +7,10 @@ for i in range(12):
     
 print("Loop ended")
 
+
+
 print("\n\n")
+print("This is Iteration :")
 
 for i in range(12):
   if( i == 10 ):
@@ -16,6 +19,31 @@ for i in range(12):
 
   print("5 X", i ,"=", 5 * i)
 
+
+
+print("\n\n")
+print("This is do while emulated: ")
+
+i = 0 
+while True:
+  print(i)
+  i += 1
+  if( i == 10 ):
+    break
+
+print("\n\n Example Number 2 Emulation  :")
+print("\n\n")
+
+
+while True:
+  print("Enter a number : ")
+  num = input()
+  if( num == "exit" ):
+    break
+  print("You entered : ", num)
+  
+print("Loop ended")
+  
 
     
 
