85c29238a21009c687dfdad9bd41b2b4a7ff1deb
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-04 14:38:45 +0000

    addd34

--- fun2.py
+++ fun2.py
@@ -23,6 +23,22 @@ avarege1(5,2,6,7,8,9)
 avarege1(1,2,3,4,5,6,7,8,9)
 # avarege1(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 
+print("\n")
+# keyword argumrnt
+def avarege2(**kwargs):
+  sum = 0
+  for i in kwargs:
+    sum = sum + kwargs[i]
+  print("The avarege of ", kwargs, " is : ", sum/len(kwargs))
+
+avarege2(a=5,b=6,c=7,d=8,e=9)
+avarege2(a=5,b=6,c=7,d=8,e=9,f=10,g=11,h=12,i=13,j=14,k=15,l=16,m=17,n=18,o=19,p=20,q=21)
+
+
+
+
+
+
 
 
 
