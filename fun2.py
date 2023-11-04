fe1601105baf9b6031cc1388dba26a1602964645
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-04 14:36:27 +0000

    adddd33

--- fun2.py
+++ fun2.py
@@ -9,4 +9,21 @@ avarege(a=5,b=2)
 avarege(a=5,b=5)
 avarege()
 
+
+print("\n")
+# variable length argumrnt
+
+def avarege1(*args):
+  sum = 0
+  for i in args:
+    sum = sum + i
+  print("The avarege of ", args, " is : ", sum/len(args))
+
+avarege1(5,2,6,7,8,9)
+avarege1(1,2,3,4,5,6,7,8,9)
+# avarege1(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+
+
+
+
   
