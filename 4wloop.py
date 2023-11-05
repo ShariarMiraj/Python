90fa79eb1103554140144c09bfb155ed406e84f9
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-05 14:41:55 +0000

    addddd38

--- 4wloop.py
+++ 4wloop.py
@@ -1,3 +1,10 @@
-food = ["pizza", "burger", "pasta", "chicken", "fish"]
-for item in food:
-  print(item)
+# food = ["pizza", "burger", "pasta", "chicken", "fish"]
+# for item in food:
+#   print(item)
+
+food = input("Enter your favorite food: ")
+while (food != "pizza"):
+  print("That's not my favorite food")
+  food = input("Enter your favorite food: ")
+print("That's my favorite food!")
+print("Bey.....!")
