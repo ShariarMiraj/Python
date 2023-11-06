5031e09d04003538cdb2c2a36a8e5a57fa1799cc
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-06 16:29:25 +0000

    adddd39

---
+++ fun3.py
@@ -0,0 +1,20 @@
+def factorial(n):
+  if n == 0:
+      return 1
+  else:
+      return n * factorial(n - 1)
+
+# Get user input
+try:
+  num = int(input("Enter a non-negative integer: "))
+  if num < 0:
+      print("Please enter a non-negative integer.")
+  else:
+      result = factorial(num)
+      print(f"The factorial of {num} is {result}")
+except ValueError:
+  print("Invalid input. Please enter a non-negative integer.")
+
+# Output:
+# Enter a non-negative integer: 5
+# The factorial of 5 is 120
