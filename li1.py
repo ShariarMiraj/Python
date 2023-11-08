9c4a3c895ec467afaabba0187e024da61e58010f
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-08 14:43:11 +0000

    addddd45

--- li1.py
+++ li1.py
@@ -9,4 +9,51 @@ print(l[-1])
 print(l[-2])
 print(type(l))
 
+ # Ex:2
+list = [1,2,3]
+list.append(4)
+print(list)
+list.append(5)
+print(list)
+list.append(6)
+print(list)
+list.append(7)
+print(list)
+list.append(8)
+print(list)
+list.append(9)
+print(list)
+list.append(10)
+print(list)
+
+# 
+print("\n\n")
+
+# EX: 3
+list1 =[1,2,3]
+list2 =[4,5,6]
+list3 = list1 + list2
+print(list3)
+
+# EX: 4
+list1 =[1,2,3]
+list2 =["miraj"]
+list3 = list1 + list2
+print(list3)
+print(list2)
+print(list1)
+
+
+print("\n\n")
+
+# EX: 5
+marks =[3,5,6, "harry", True]
+print(marks[-3])
+print(marks[len(marks)-3])
+print(marks[len(marks)-2])
+print(marks[len(marks)-1])
+print(marks[5-3])
+print(marks[2])
+
+
 
