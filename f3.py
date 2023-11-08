3fc9615f22a647e24f7bbbd5e6c38d1577a70f8e
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-08 14:20:14 +0000

    addddd43

--- f3.py
+++ f3.py
@@ -1,4 +1,28 @@
+# EXMPLE OF DEFULT FUNCTION
 def name(fname, mname= "Jhon" , lname="Whatson"):
   print("Hello" , fname , mname , lname)
 
 name("Amy", "Agarwal" , "jain")
+
+name("Amy", "Agarwal")
+name("Amy")
+# name("Amy", "Agarwal")
+# name("Amy", "Agarwal", "jain")
+name("")
+
+# EXMPLE OF KEYWORD FUNCTION
+def name(fname, mname= "Jhon" , lname="Whatson"):
+  print("Hello" , fname , mname , lname)
+
+# AVERAGE
+def average(*numbers):
+  sum = 0
+  for num in numbers:
+    sum = sum +num
+  print("Averge number is ", sum/len(numbers))
+
+average(5,6,7,1)
+  
+
+
+
