4914d56b3206eb40a510914ebe4d2eea4a28b76d
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-09 15:22:47 +0000

    adddd50
    Tuple

---
+++ tp1.py
@@ -0,0 +1,15 @@
+tuple = (1,2,3,4,5)
+print(tuple)
+print(tuple[0])
+print(tuple[-1])
+# print(tuple[1:3])
+# print(tuple[:3])
+# print(tuple[3:])
+# print(tuple[:])
+# print(tuple[::2])
+# print(tuple[::-1])
+
+if 4 in tuple:
+    print("Yes")
+else:
+    print("No")
