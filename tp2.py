9678ed923083d97c7dd9c4a63b82b4563946306b
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-10 12:14:30 +0000

    adddd52

--- tp2.py
+++ tp2.py
@@ -1,9 +1,24 @@
 # Manipulating; tuples:
 
-continues = ("Bangladesh","India","China","Pakistan","Nepal")
+continues = ("Bangladesh", "India", "China", "Pakistan", "Nepal")
 temp = list(continues)
-temp.append("Bhutan")    # add items
-temp.pop(3)              # remove items
-temp[2] = "Finland"      # replace items or change 
+temp.append("Bhutan")  # add items
+temp.pop(3)  # remove items
+temp[2] = "Finland"  # replace items or change
 continues = tuple(temp)
 print(continues)
+
+# EX ;2
+
+continues = ("Bangladesh", "India", "China", "Pakistan", "Nepal")
+continues2 = ("Canada", "USA", "Mexico")
+southaisa = continues + continues2
+print(southaisa)
+
+print("\n\n")
+# EX ; 3 .. how come the numbr
+
+tuple1 = (1, 2, 3, 4, 5, 5)
+res = tuple1.count(5)
+res = tuple1.index(3)
+print("Couting 5 : ", res)
