b117c815d94a0610e8df0cc7bda548f081c66375
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-11 15:32:45 +0000

    adddd55
    Docstrings in Python

---
+++ dstri.py
@@ -0,0 +1,22 @@
+def squre(n):
+  "take in a number n, returns the square of n "
+  print(n**2)
+squre(5)
+print(squre.__doc__)
+
+
+print("\n\n")
+
+def add(x, y):
+  """
+  Adds two numbers together.
+
+  Parameters:
+  x (int): The first number.
+  y (int): The second number.
+
+  Returns:
+  int: The sum of x and y.
+  """
+  return x + y
+print(add.__doc__)
