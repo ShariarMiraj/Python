ede7bcf8367c6385adf7e22325ca106c8ba0bf92
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-13 16:04:07 +0000

    addddd222

---
+++ emni.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+  fib_sequence = [0, 1]
+
+  for i in range(2, n):
+      next_term = fib_sequence[i-1] + fib_sequence[i-2]
+      fib_sequence.append(next_term)
+
+  return fib_sequence
+
+# Change the value of 'n' to control the length of the sequence
+n = 10  # You can change this to any positive integer
+fib_sequence = fibonacci(n)
+
+print(f"Fibonacci Sequence of length {n}: {fib_sequence}")
+# Output: Fibonacci Sequence of length 10: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
