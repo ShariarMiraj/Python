00cc3707100a6721ecadfa05674304960c50a7db
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-14 13:45:59 +0000

    adddd53

---
+++ loop.py
@@ -0,0 +1,13 @@
+# Example 1: Iterate over a range of numbers
+for i in range(5):
+    print(i)
+
+# Example 2: Iterate over elements in a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Example 3: Iterate over characters in a string
+word = "Python"
+for char in word:
+    print(char)
