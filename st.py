7c453bc3ad1492cde981588dc8fb2c667284c121
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-15 15:19:08 +0000

    adddd57

--- st.py
+++ st.py
@@ -16,3 +16,30 @@ my_st.update([6, 7, 8])
 my_st.remove(6)
 my_st.discard(7)
 print(my_st)
+
+# Union method
+cites= {"New York", "London", "Paris", ""}
+cities2={"New York", "London", "Paris", "Tokyo"}
+cities3=cites.union(cities2)
+citie4=cites.intersection(cities2)
+print(cities3)
+print(citie4)
+
+# symmetic difference
+cites= {"New York", "London", "Paris", ""}
+cities2={"New York", "London", "Paris", "Tokyo"}
+cities3=cites.symmetric_difference(cities2)
+print(cities3)
+
+print(" ")
+# intersection
+cites= {"New York", "London", "Paris", ""}
+cities2={"New York", "London", "Paris", "Tokyo"}
+cities3=cites.intersection(cities2)
+print(cities3)
+
+print(" ")
+# isdisjoint
+cites= {"New York", "London", "Paris", ""}
+cities2={"New York", "London", "Paris", "Tokyo"}
+print(cites.isdisjoint(cities2))
