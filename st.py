b3577306bd67a34851ec9ad846cbf120045080a7
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-15 15:05:30 +0000

    adddd56

--- st.py
+++ st.py
@@ -10,3 +10,9 @@ class MyClass:
   def my_instance_method(self):
       print("This is an instance method")
 
+my_st = set([1, 2])
+my_st.add(6)
+my_st.update([6, 7, 8])
+my_st.remove(6)
+my_st.discard(7)
+print(my_st)
