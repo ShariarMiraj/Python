48d0ed463d10f8ea3ba375d92ab39bcf84f6271c
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-16 15:23:23 +0000

    adddd59
    Dictionaries in Python

---
+++ dic.py
@@ -0,0 +1,46 @@
+# Dictionaries are ordered collections of key-value pairs.
+# Dictionaries are mutable.
+# Dictionaries are iterable.
+# Dictionaries are indexed by keys.
+# Dictionaries are indexed by keys.
+
+dic = {
+  334:"Miraj",
+  "B": "Shahriar",
+  "C": "AMMU",  
+}
+
+print(dic[334])
+print(dic["B"])
+print(dic["C"])
+
+info = {
+  "Name": "Miraj",
+  "Age": 22,
+  "Gender": "Male",
+  "Occupation": "Student",
+  "City": "Pune",
+  "State": "Maharashtra",
+  "Country": "India",
+  "Phone": "9876543210",
+  "Email": "xcvkp@example.com",
+  "Address": "Pune, Maharashtra, India",
+  "DOB": "18 July 2001",
+  "Height": "5', 7",
+  "Weight": "60 kg",
+  "Marital Status": "Single",
+  "Languages": "English, Hindi, Urdu",
+  "Hobbies": "Reading, Writing, Travelling",
+  "Interests": "Music, Movies, Travelling",
+  "Favorite Food": "Pizza, Burger, Pasta",
+  "Favorite Color": "Blue, Red, Green",
+  "Favorite Subject": "Maths, Science, Social Science",
+  "Favorite Book": "Harry Potter, Lion King, Diary of a Wimpy Kid",
+  "Favorite Movie": "The Matrix, Harry Potter, Diary of a Wimpy Kid",
+  "Favorite Quote": "The Matrix, Harry Potter, Diary of a Wimpy Kid",
+  "Favorite Song": "The Matrix, Harry Potter, Diary of a Wimpy Kid",
+  "Favorite Band": "The Matrix, Harry Potter, Diary of a Wimpy Kid",
+  "Favorite Actor": "The Matrix, Harry Potter, Diary of a Wimpy Kid",
+
+  }
+print(info)
