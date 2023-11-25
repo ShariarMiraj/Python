3ce8b149988624e68914ebe5ff996df381f24373
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-25 14:25:14 +0000

    addd67
    Dictionary Methods in Python

--- dc4.py
+++ dc4.py
@@ -1,9 +1,11 @@
 # Dictionary Methods in Python
 
 ep1 = { 122 : 45, 125 : 89}
-ep2 = { 125 : 45, 125 : 89}
+ep2 = { 225 : 41, 11112 : 88}
 # ep3 = { 125 : 45, 125 : 89}
 
-ep1 = update(ep2)
+ep1.update(ep2)
+# ep1.pop(125)
+ep1.popitem()
 print(ep1)
 # ep1 = update(ep1, ep3)
