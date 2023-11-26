614a621ca5c175d282da3c35a25a0b6081e77540
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-26 15:42:53 +0000

    adddd69

--- fp1.py
+++ fp1.py
@@ -8,3 +8,26 @@ for i in range(6):
 # after breaK THE ELSE IS NOT PRINT ANHY MORE 
 else:
   print("i is smaller than 6")
+
+print("\n")
+# same this for while loop
+
+i = 0
+while i <6:
+  print(i)
+  i = i + 1
+  if i == 3:
+    break
+    
+else:
+  print("i is smaller than 6")
+
+
+#  Exmple _ 3
+
+print(" \n")
+
+for x in range (5):
+  print("iteration no {}  in for loop".format(x+1))
+else:
+  print("Out of loop")
