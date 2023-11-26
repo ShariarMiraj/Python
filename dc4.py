268f7ee7110122465cfe01267e5c0d1966b8fcc9
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-26 15:31:40 +0000

    addd68
    For Loop with else in Python

--- dc4.py
+++ dc4.py
@@ -7,5 +7,6 @@ ep2 = { 225 : 41, 11112 : 88}
 ep1.update(ep2)
 # ep1.pop(125)
 ep1.popitem()
+del ep1[125]
 print(ep1)
 # ep1 = update(ep1, ep3)
---
+++ fp1.py
@@ -0,0 +1,10 @@
+# for Loop with else in Python
+
+for i in range(6):
+  print(i)
+  if i == 3:
+    break
+
+# after breaK THE ELSE IS NOT PRINT ANHY MORE 
+else:
+  print("i is smaller than 6")
