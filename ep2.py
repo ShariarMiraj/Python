94df84fe9ee9c6118e53af804075493d14286a8c
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-27 07:38:08 +0000

    addd73
    Exception Handling in Python with if else

---
+++ ep2.py
@@ -0,0 +1,32 @@
+# Exception Handling in Python with if else 
+
+# try:
+#   # Code that may raise an exception
+#   x = int(input("Enter a number: "))
+#   result = 10 / x
+# except ValueError:
+#   # Handle the case when the input is not a valid integer
+#   print("Invalid input. Please enter a valid number.")
+# except ZeroDivisionError:
+#   # Handle the case when the user enters 0
+#   print("Cannot divide by zero.")
+# else:
+#   # Code to be executed if no exception is raised
+#   print("Result:", result)
+# finally:
+#   # Code that will be executed no matter what
+#   print("Execution complete.")
+
+try:
+  num =int(input("Enter a number:"))
+  result = 10 / num
+except ValueError:
+  print("Invalid number tpye")
+
+except ZeroDivisionError:
+  print("Zero cannot be divided any time ")
+else:
+  print("This thee result :", result)
+
+finally:
+  print("This is the end of the program")
