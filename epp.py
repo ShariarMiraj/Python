8c4d7575f91ec4667dd7f222b2cf911edcf8dd89
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-27 07:24:10 +0000

    adddd72

--- epp.py
+++ epp.py
@@ -1,12 +1,32 @@
 # Exception Handling in Python
 
-b = input("Enter a number: ")
-print(f"Multipliction table {b} is :")
+# b = input("Enter a number: ")
+# print(f"Multipliction table {b} is :")
+
+# try:
+#   for i in range(1, 11):
+#     print(f"{int(b)} X {i} = {int(b)*i}")
+# except:
+#   print("Invalid number..!")
+
+
+# print("End the program...")
+
+
+# Syntax Error :
+
+# try:
+#   num = int(input("Enter a number: "))
+
+# except ValueError:
+#   print("Number the enterd is not an integer")
+
+
+# Index error :
 
 try:
-  for i in range(1, 11):
-    print(f"{int(b)} X {i} = {int(b)*i}")
-except:
-  print("Invalid number..!")
-
-  
+  num = int(input("Enter a number: "))
+  a=[6,3]
+  print(a[num])  
+except IndexError:
+  print("Index out of range")
