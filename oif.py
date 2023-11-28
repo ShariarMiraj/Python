1488b5abd23b081d3b66b2e5fbd1387ae2151c89
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-28 14:01:10 +0000

    addddd78

--- oif.py
+++ oif.py
@@ -1,7 +1,28 @@
 # Short hand if else statements 
-a = int(input("Enter the number A :"))
-b=  int(input("Enter the Secondo B :"))
-print("A") if a >b else print("B")
+# a = int(input("Enter the number A :"))
+# b=  int(input("Enter the Secondo B :"))
+# print("A") if a >b else print("B")
 
-c = 9 if a>b else 0
-print(c)
+# c = 9 if a>b else 0
+# print(c)
+
+
+# String Concatenation:
+
+is_even = lambda num: "Even" if num % 2 == 0 else "Odd"
+print(is_even(7))
+
+# List Comprehension:
+numbers = [1, 2, 3, 4, 5]
+squared = [x**2 if x % 2 == 0 else x for x in numbers]
+print(squared)
+
+# Dictionary Assignment:
+age = 25
+category = "Young" if age < 30 else "Adult"
+print(category)
+
+# Short Circuiting:
+value = 15
+result = "Valid" if 10 < value < 20 else "Invalid"
+print(result)
