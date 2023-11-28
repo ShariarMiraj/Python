6a3c0cdf727f33befba8322bc0aa1b4380195a34
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-11-28 13:45:45 +0000

    adddd77

---
+++ oif.py
@@ -0,0 +1,7 @@
+# Short hand if else statements 
+a = int(input("Enter the number A :"))
+b=  int(input("Enter the Secondo B :"))
+print("A") if a >b else print("B")
+
+c = 9 if a>b else 0
+print(c)
