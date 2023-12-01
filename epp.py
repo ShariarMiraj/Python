f730221f9708d09c5ef2e0e7a9d56a1608386e0c
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-01 13:09:29 +0000

    addddd83

--- epp.py
+++ epp.py
@@ -30,3 +30,26 @@ try:
   print(a[num])  
 except IndexError:
   print("Index out of range")
+
+
+# Another code 
+
+def divide_numbers(a, b):
+  try:
+      result = a / b
+      print("Result of division:", result)
+  except ZeroDivisionError:
+      print("Error: Division by zero is not allowed.")
+  except TypeError:
+      print("Error: Please provide valid numeric values.")
+  except Exception as e:
+      print(f"An unexpected error occurred: {e}")
+  else:
+      print("Division operation completed successfully.")
+  finally:
+      print("This block always executes, regardless of whether an exception occurred.")
+
+# Example usages
+divide_numbers(10, 2)  # Normal case
+divide_numbers(5, 0)   # Division by zero
+divide_numbers("abc", 2)  # TypeError
