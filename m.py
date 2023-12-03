470ef4ebbf64c95a79abb97ceab276db24e8e364
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-03 07:14:32 +0000

    adddd85
    How import works in Python

--- m.py
+++ m.py
@@ -1,3 +1,41 @@
-import miraj
+# import m
 
-miraj.welcome()
+# m.welcome()
+
+# miraj.welcome()
+
+from math import sqrt,pi 
+#  in here sqrt is a funtion and pi is the variable...
+result = sqrt(9) * pi
+print(result)
+
+print("\n")
+#  or 
+
+from math import * 
+
+result = sqrt(9) * pi
+print(result)
+
+print("\n")
+
+#  or , this called as keyword  
+
+from math import pi ,sqrt as s  
+
+result = s(9) * pi
+print(result)
+
+# or , as exmple
+
+import math  as m 
+
+result = m.sqrt(8) * m.pi
+print(result)
+
+print("\n")
+# dir function 
+
+import math 
+
+print(dir(math))
