3c37360ce2260edc717514b4d892b58cd814c0d8
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-04 13:36:24 +0000

    addddd91

--- ex4.py
+++ ex4.py
@@ -2,15 +2,15 @@ st = input("Enter massege")
 words = st.split(" ")
 coding =True
 if(coding):
+  
   nwords = []
-   for word in words:
-  if(len(st)>=3):
+  for word in words:
+    if(len(word)>=3):
     r1 = "dsf"
     r2 = "jkr"
     stnew = r1 + word[1:] + word[0] + r2
     nwords.append(stnew)
-
-  print(" ".join(nwords)))
+  print(" ".join(nwords))
 
 else:
   pass
