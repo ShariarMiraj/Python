3dc3f5471f3e246f51d3b120c6799708126def10
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-07 17:40:09 +0000

    addddd97
    Simple !! marks calculator for student

--- read.py
+++ read.py
@@ -1,7 +1,31 @@
-# read(), readlines() and other methods | Python
+# # read(), readlines() and other methods | Python
+# f = open('myf.txt', 'r')
+# while True:
+#   line = f.readline()
+#   if not line:
+#     # print(line, type(line))
+#     break
+#   print(line)
+
+
+print("\n\n")
+# smple !!marks calcultor for student
+
 f = open('myf.txt', 'r')
+i = 0
 while True:
+  i = i + 1
   line = f.readline()
   if not line:
     break
+    
+  s1 = line.split(",")[0]
+  s2 = line.split(",")[1]
+  s3 = line.split(",")[2]
+  print(f"Marks of student {i} in Maths is: {s1}")
+  print(f"Marks of student {i} in English is: {s2}")
+  print(f"Marks of student {i} in SST is: {s3}")
+ 
   print(line)
+
+    
