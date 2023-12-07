9948c67f22e9b8aae28c66cdc9800b5582208479
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-07 17:11:18 +0000

    adddd
    File IO in Python |

--- i.py
+++ i.py
@@ -7,7 +7,7 @@
 
 # Exmple-1 ....Reading form a file..
 
-f = open('myfile.txt', 'r')
+f = open('myfl.txt', 'r')
 # print(f)
 txt = f.read()
 print(txt)
@@ -17,9 +17,9 @@ f.close()  # if f.close it not called ..thn it will not work and print it  <_io.
 
 # Exmple-2 ....Wrting form a file..
 
-f = open('myfile.txt', 'w')
+f = open('myfl.txt', 'w')
 f.write('Hello, world!...')
 f.close()
 
-with open('myfile.txt', 'a') as f:
+with open('myfl.txt', 'a') as f:
   f.write("Hey I am inside with")
