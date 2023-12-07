5da629d1eccb8ccbbe32ffb63b3ff02faa8c8058
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-07 15:53:19 +0000

    addddd

--- io.py
+++ io.py
@@ -3,3 +3,10 @@
 # 'r' for reading 
 # 'w' for writing
 # 'a' for appending
+
+f = open('myfile.txt', 'r')
+# print(f)
+txt = f.read()
+print(txt)
+f.close()  # if f.close it not called ..thn it will not work and print it  <_io.TextIOWrapper name='myfile.txt' mode='r' encoding='UTF-8'>
+# hello mieaj ...who are u ?..
