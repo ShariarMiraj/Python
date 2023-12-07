d057bb73fd1d0a06d3eee1de69c719fa44d25411
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-07 17:01:28 +0000

    adddd95

--- io.py
+++ io.py
@@ -1,8 +1,11 @@
 # File IO in Python
 
-# 'r' for reading 
+# 'r' for reading   and (r) mode is defelut mode
 # 'w' for writing
 # 'a' for appending
+
+
+# Exmple-1 ....Reading form a file..
 
 f = open('myfile.txt', 'r')
 # print(f)
@@ -10,3 +13,13 @@ txt = f.read()
 print(txt)
 f.close()  # if f.close it not called ..thn it will not work and print it  <_io.TextIOWrapper name='myfile.txt' mode='r' encoding='UTF-8'>
 # hello mieaj ...who are u ?..
+
+
+# Exmple-2 ....Wrting form a file..
+
+f = open('myfile.txt', 'w')
+f.write('Hello, world!...')
+f.close()
+
+with open('myfile.txt', 'a') as f:
+  f.write("Hey I am inside with")
