62e7f45f4160692163f904005a793a92dca2d8a4
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-07 17:47:47 +0000

    addddddd98

--- read.py
+++ read.py
@@ -28,4 +28,12 @@ while True:
  
   print(line)
 
-    
+
+# EX-3
+# writeline 
+f = open('myf2.txt' ,'w')
+lines = ['line 1\n', 'line 2\n', 'line 3\n']
+for line in lines:
+  f.write(line + '\n')
+
+f.close()
