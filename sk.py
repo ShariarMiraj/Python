133bb431508bec1896a4051beea142a54bed4e34
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-08 12:48:38 +0000

    addddd100

--- sk.py
+++ sk.py
@@ -10,3 +10,14 @@ with open('file.txt' , 'r') as f:
   data = f.read(5)
   print(data)
 
+# tell() function returns the current position within the file, in bytes.
+
+# truncate()
+
+with open('smile.txt', 'w') as f:
+  f.write('Hello World!')
+  f.truncate(5)
+
+# for check or print  wittn option
+with open( 'smile.txt', 'r') as f:
+  print(f.read())
