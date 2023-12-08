6e5e947ba8120ae629a7459c70cf00d620e3ea4d
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-08 13:31:53 +0000

    adddd

--- lam.py
+++ lam.py
@@ -3,3 +3,11 @@
 add = lambda x, y: x + y
 result = add(3, 5)
 print(result)  # Output: 8
+
+
+double = lambda x:  x*x*x
+cube = lambda x: x*2
+avg = lambda x, y, z: (x + y + z) / 2
+print(double(5))
+print(cube(5))
+print(avg(3, 5, 10))
