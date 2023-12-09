18c287e3b33179821c59c19e525f6b2e058b4c72
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-09 17:45:56 +0000

    adddddd106
    'is' vs '==' in Python

---
+++ iseq.py
@@ -0,0 +1,8 @@
+# 'is' vs '==' in Python
+# 'is' and '==' are both comparison operators in Python. The 'is' operator compares the identity of two objects, while the '==' operator compares the values of the objects.
+
+a = "miraj"
+b = "miraj"
+
+print( a is b) # exact location of object in memory
+print( a == b) # value
