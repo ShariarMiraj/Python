4ea7189d140fb80f5476b9c135e695f6a32b12ab
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-09 17:19:05 +0000

    addd

--- map.py
+++ map.py
@@ -21,3 +21,13 @@ numbers = [1, 2, 3, 4, 5]
 # Example: Sum all the numbers in the list
 sum_of_numbers = reduce(lambda x, y: x + y, numbers)
 print(sum_of_numbers)  # Output: 15
+
+
+# Filter()
+
+l = [1, 2, 4, 6, 4, 6]
+def filter_function(a):
+  return a > 5
+
+newnewl = list(filter(filter_function, l))
+print(newnewl)
