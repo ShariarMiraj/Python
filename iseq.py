6936400ef968a7590ebef8b12ff9ed048bdbdcca
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-09 17:48:25 +0000

    adddd

--- iseq.py
+++ iseq.py
@@ -6,3 +6,9 @@ b = "miraj"
 
 print( a is b) # exact location of object in memory
 print( a == b) # value
+
+c= [1,2,3,4]
+d= [1,2,3,4]
+
+print(c is d) # exact location of object in memory  
+print( c == d) # value
