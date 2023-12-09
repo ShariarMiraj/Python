96b0036f3e2f15a94d02a0ddf9540500272bdfcd
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-09 17:14:41 +0000

    adddd104

--- map.py
+++ map.py
@@ -1,7 +1,7 @@
 # Map, Filter and Reduce in Python
 
 def cube(x):
-  return x * x * x
+  return x * x
 
 print(cube(2))
 
@@ -9,3 +9,15 @@ l = [1, 2, 4, 6, 4, 3]
 
 newl = list(map(cube, l))
 print(newl)
+
+
+print('\n')
+
+from functools import reduce
+
+# Syntax: reduce(function, iterable[, initializer])
+numbers = [1, 2, 3, 4, 5]
+
+# Example: Sum all the numbers in the list
+sum_of_numbers = reduce(lambda x, y: x + y, numbers)
+print(sum_of_numbers)  # Output: 15
