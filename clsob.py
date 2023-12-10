edd02312bdfb8be03bd5d82c702eeca405855e8b
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-10 11:09:34 +0000

    addd107
    Classes and Objects in Python

---
+++ clsob.py
@@ -0,0 +1,18 @@
+# Classes and Objects in Python
+class Person:
+  name = "miraj"
+  occupation = "software developer"
+  univercity ="AIUB"
+
+  def info(self):
+    print(f"{self.name} is a {self.occupation} from {self.univercity}")
+
+
+
+a=Person()
+a.info()
+# print(a.name)
+# print(a.occupation)
+# print(a.univercity)
+
+  
