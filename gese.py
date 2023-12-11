71cf58da70b5fcd58cfbd3776a54e3b73f4bc1ae
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-11 16:20:56 +0000

    adddd110
    Getters and Setters in Python

---
+++ gese.py
@@ -0,0 +1,44 @@
+# Getters and Setters in Python 
+
+# Getter- a function that returns the value of a private variable
+class Myclass:
+  def __init__(self,value):
+    self._value = value
+
+
+  def show(self):
+    print(f"Value is {self._value}")
+  
+  @property
+  def value(self):
+    return  self._value
+
+
+obj = Myclass(10)
+obj.show()
+print(obj.value)
+
+
+
+# Setter- a function that sets the value of a private variable
+
+class Myclass:
+  def __init__(self,value):
+    self._value = value
+
+  def show(self):
+    print(f"Value is {self._value}")
+
+  # Getter
+  @property  
+  def value(self):
+    return  self._value
+
+  @value.setter  # decorater
+  def value(self,new_value):
+    self._value = new_value/10
+
+obj = Myclass(10)
+obj.value = 50
+obj.show()
+
