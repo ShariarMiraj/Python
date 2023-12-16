83da437d7367a5305b6126c2f2df87a1d51ec630
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-16 13:00:19 +0000

    adddd

--- sta.py
+++ sta.py
@@ -1,2 +1,12 @@
 # Static Methods in Python 
 
+class MyClass:
+  class_variable = "I am a class variable"
+
+  def __init__(self, instance_variable):
+      self.instance_variable = instance_variable
+
+  def instance_method(self):
+      print(f"I am an instance method. Instance variable: {self.instance_variable}")
+
+ 
