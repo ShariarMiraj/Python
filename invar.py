fb81701b72f0361db42e297740ed8aa8e6c2ac67
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2023-12-26 16:14:18 +0000

    adddd

--- invar.py
+++ invar.py
@@ -1,10 +1,24 @@
-# Instance variables vs Class variables in Python 
+# Instance variable  vs Class variables in Python 
 class Employee:
+  comapnyName = "Tata"  # Class variables
+  # noOfEmployees = 0
   def __init__(self,name):
     self.name = name
+    self.amout = 0.2
+    # Employee.noOfEmployees +=1
 
   def ShowInfo(self):
-    print(f"The name of the employee is {self.name}")
+    print(f"The name of the employee is : {self.name} and the amount in {self.comapnyName} is {self.amout}")
+
+  # print(f"The name of the employee is : {self.name} and the amount in {self.noOfEmployees} size{self.comapnyName} is {self.amout}")
 
 emp1 = Employee("MIraj")
+emp1.amout =50
+emp1.comapnyName = "Google" # Insatance variable
 emp1.ShowInfo()
+Employee.comapnyName = "Nestle"
+print(Employee.comapnyName)
+
+emp2 = Employee("Rohan")
+emp2.comapnyName = "cocacola"
+emp2.ShowInfo()
