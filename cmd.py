c2471ab2b012fa922ce425cae3300de60138b932
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2024-01-04 13:09:08 +0000

    Creating command line utility in python
    Creating command line utility in python

---
+++ cmd.py
@@ -0,0 +1,12 @@
+# Creating command line utility in python 
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# add commd line arguments
+parser.add_argument("arg1", help="description of argument 1")
+parser.add_argument("arg2", help="description of argument 2")
+
+# parse the arguments
+args = parser.parse_args()
