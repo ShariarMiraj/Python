95f7d2470d387190f451c91f9d3493faec84d24d
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2024-01-05 14:42:29 +0000

    Exercise 9: Solution - Shoutouts to Everyone
    Exercise 9: Solution - Shoutouts to Everyone

--- sho.py
+++ sho.py
@@ -1,7 +1,19 @@
 # Exercise 9: Solution - Shoutouts to Everyone
+#  for MAC
+# from os import system
+# names = ["Rahul","Nishant","Harry"]
 
-from os import system
-names = ["Rahul","Nishant","Harry"]
+# for name in names:
+#   system(f'Say Shoutout to {name}')
 
-for name in names:
-  system(f'Say Shoutout to {name}')
+
+
+def shoutouts(names):
+  for name in names:
+      print(f"Shoutout to {name}!")
+
+# List of names
+people = ["Alice", "Bob", "Charlie", "David", "Eva"]
+
+# Call the function with the list of names
+shoutouts(people)
