ce6eaea0dea2f67bddd0740945146aaad3ffc7d1
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2024-01-05 14:21:55 +0000

    Shutil Module in Python
    Shutil Module in Python

---
+++ shu.py
@@ -0,0 +1,9 @@
+# Shutil Module in Python
+#  is it a higeh level oprations 
+
+import  shutil
+
+shutil.copy("main.py","main2.py")
+shutil.copytree(".tutorial","mytutorial")
+shutil.move(".tutorial/file.file","file.file")
+shutil.rmtree("mytutorial")
