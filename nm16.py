e2883f2398eb70ad9003b6f1d77a0a317b0a27f7
ShariarMiraj <87632704+ShariarMiraj@users.noreply.github.com>
2024-01-24 14:44:35 +0000

    (NUMpy) Solving function using by linalg
    (NUMpy) Solving function using by linalg

---
+++ nm16.py
@@ -0,0 +1,14 @@
+# (NUMpy) Solving function using by linalg
+
+import numpy as np 
+
+a = np.array([[1,1], [1.5,4.0]])
+b = np.array([2200,5050])
+
+x =np.linalg.inv(a).dot(b)
+print(x)
+
+#  For this we using the solve function cuz its better than inv function
+
+y = np.linalg.solve(a,b)
+print(y)
